963b4424336134942da5c91c3a74b4adc4c55159
ANUSHI JAIN <jainanushi63@gmail.com>
2026-02-07 13:26:57 +0530

    Update Success.js

--- Stdmngmt/fromt/src/Success.js
+++ Stdmngmt/fromt/src/Success.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { PiStudentFill } from "react-icons/pi";
 import { FaBookReader } from "react-icons/fa";
-import { TiArrowBack } from "react-icons/ti";
-import { TiArrowForward } from "react-icons/ti";
+import { TiArrowBack, TiArrowForward } from "react-icons/ti";
 import { GiNotebook } from "react-icons/gi";
 import { FaPenClip } from "react-icons/fa6";
-export const Success = () => {
-  const navigate = useNavigate()
-  const click = ()=>{
-navigate('/view')
-  }
-  const back =()=> {
-    window.location.href = '/form'; 
-  }
-  return (
-    <>
-    <div className="success-page">
-    <h1 className='icon'>  
-      <TiArrowBack onClick={back}/>
-      <PiStudentFill/>
-      <TiArrowForward/>
-    </h1>
-    <div className='success'> <h1>Record Added Successfuly!!</h1> </div>
-    <button onClick={click}>View Record</button>
 
-    <div>
-    <h1 className='successicon'>
-    <GiNotebook/>
-      <FaBookReader/>
-      <FaPenClip/>
-     </h1> 
+export const Success = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="success-page">
+      <h1 className="icon">
+        <TiArrowBack onClick={() => navigate("/form")} />
+        <PiStudentFill />
+        <TiArrowForward />
+      </h1>
+
+      <div className="success">
+        <h1>Record Added Successfully!!</h1>
+      </div>
+
+      <button onClick={() => navigate("/view")}>View Record</button>
+
+      <h1 className="successicon">
+        <GiNotebook />
+        <FaBookReader />
+        <FaPenClip />
+      </h1>
     </div>
-    </div>
-    </>
-  )
-}
+  );
+};
