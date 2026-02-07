0c33ad33a9e1515383a864373b8346f9ff25ba04
ANUSHI JAIN <jainanushi63@gmail.com>
2026-02-07 13:37:11 +0530

    Update Log.js

--- Stdmngmt/fromt/src/Log.js
+++ Stdmngmt/fromt/src/Log.js
@@ -2,66 +2,51 @@ import React, { useState, useEffect } from "react";
 import "./log.css";
 import { useNavigate } from "react-router-dom";
 
-/* 🔴 IMPORT IMAGES FROM SRC */
-import photo from "../Image/photo.jpg";
-import img from "../Image/img.webp";
-import imag2 from "../Image/imag2.jpeg";
+import photo from "./Image/photo.jpg";
+import img from "./Image/img.webp";
+import imag2 from "./Image/imag2.jpeg";
 
 export const Log = () => {
   const navigate = useNavigate();
 
-  /* image slider */
   const images = [photo, img, imag2];
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const slider = setInterval(() => {
       setIndex((prev) => (prev + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
+    }, 2500);
+    return () => clearInterval(slider);
   }, []);
 
-  const handleLoginClick = () => {
-    navigate("/form");
-  };
-
   return (
-    <>
-      <h1 style={{ textAlign: "center", fontSize: "60px" }}>
+    <div className="home-page">
+      <h1 className="main-title">
         Welcome To Student Management Database
       </h1>
 
-      <div className="container">
-        <div className="row">
-
-          {/* IMAGE BOX */}
-          <div
-            className="imgbox"
-            style={{ backgroundImage: `url(${images[index]})` }}
-          ></div>
-
-          {/* TEXT */}
-          <div className="col m3">
-            <h5 style={{ textAlign: "justify", fontSize: "20px" }}>
-              Student management is the process of managing student data,
-              including personal information, grades, attendance...
-            </h5>
-          </div>
-        </div>
+      {/* IMAGE */}
+      <div className="imgbox">
+        <img src={images[index]} alt="student" />
       </div>
 
-      <button onClick={handleLoginClick}>LOGIN</button>
+      {/* TEXT */}
+      <p className="desc">
+        Student management is the process of managing student data including
+        personal information, grades and attendance.
+      </p>
+
+      {/* BUTTON */}
+      <button onClick={() => navigate("/form")} className="login-btn">
+        LOGIN
+      </button>
 
       <p>
-        Don't have an account?
-        <span
-          onClick={() => navigate("/form")}
-          style={{ color: "#00a69c", cursor: "pointer" }}
-        >
-          <b> Sign Up</b>
+        Don't have an account?{" "}
+        <span onClick={() => navigate("/form")} className="signup">
+          Sign Up
         </span>
       </p>
-    </>
+    </div>
   );
 };
