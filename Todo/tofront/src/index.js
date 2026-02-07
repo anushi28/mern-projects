75ff717fcfd47dca357d8b04e5f381bdaa037831
ANUSHI JAIN <jainanushi63@gmail.com>
2026-02-07 17:08:37 +0530

    Refactor index.js to simplify rendering structure

--- Todo/tofront/src/index.js
+++ Todo/tofront/src/index.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Main } from './Main';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Create } from './Create';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./todo.css";   // your main styling file
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<BrowserRouter>
-<Routes>
-  <Route path='/' element={<Main></Main>}></Route>
-  <Route path='/Create' element={<Create/>}/>
-
-  <Route path = '/app' element ={<App/>}/>
-</Routes>
-</BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
-
