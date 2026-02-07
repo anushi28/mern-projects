8d635272043deca2a605db6d692ad7d201b628b7
ANUSHI JAIN <jainanushi63@gmail.com>
2026-02-07 13:30:05 +0530

    Refactor Log component with image slider and cleanup

--- Stdmngmt/fromt/src/Log.js
+++ Stdmngmt/fromt/src/Log.js
@@ -1,54 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./log.css";
+import { useNavigate } from "react-router-dom";
 
-import './log.css'
-import { useNavigate } from 'react-router-dom'
+import img1 from "../Image/photo.jpg";
+import img2 from "../Image/img.webp";
+import img3 from "../Image/imag2.jpeg";
 
 export const Log = () => {
   const navigate = useNavigate();
+
+  const images = [img1, img2, img3];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const handleLoginClick = () => {
-    navigate("/form"); // Redirect to the form page
+    navigate("/form");
   };
+
   return (
     <>
-   
-   <h1 style={{ textAlign: 'center', fontSize: '70px' }}> Welcome To Student Management Database</h1>
-   <div class="container">
-      <div class="row">
-   
-        <div class="col m5" className='imgbox'>
-        
-        </div>
+      <h1 style={{ textAlign: "center", fontSize: "70px" }}>
+        Welcome To Student Management Database
+      </h1>
 
-        {/* Button Column */}
-        <div class="col m3">
-            <h5 style={{ textAlign: 'justify' , fontSize: '21px'}}>Student management is the process of managing student data, including personal information, grades, attendance, and health information, for a school or educational institution. Student management systems, also known as student information systems (SIS). <br></br>
-            Student management systems can help with a variety of tasks, including:- <br></br>
-1. Tracking student data-
-Student management systems can track a student's workload, grades, attendance, health information,and more.  <br></br>
-2. Streamlining administrative work-
-Student management systems can help streamline administrative tasks for teachers. <br></br>
-3. Improving communication-
-Student management systems can help improve communication between administrators, teachers, and the school community. <br></br>
+      <div className="container">
+        <div className="row">
 
+          {/* IMAGE SLIDER */}
+          <div
+            className="imgbox"
+            style={{ backgroundImage: `url(${images[index]})` }}
+          ></div>
 
+          {/* TEXT */}
+          <div className="col m3">
+            <h5 style={{ textAlign: "justify", fontSize: "21px" }}>
+              Student management is the process of managing student data...
             </h5>
-      
-           
-
+          </div>
         </div>
       </div>
-    </div>
 
-    <button 
-       onClick={handleLoginClick}
-     
-    >
-      LOGIN
-    </button>
+      <button onClick={handleLoginClick}>LOGIN</button>
 
-    <p>Don't have an account! <a href='/user'  style={{color:'rgb(0, 153, 144)'}}> <b>Sign Up </b></a></p>
-
-  
+      <p>
+        Don't have an account!
+        <span
+          onClick={() => navigate("/form")}
+          style={{ color: "#009990", cursor: "pointer" }}
+        >
+          {" "}
+          <b>Sign Up</b>
+        </span>
+      </p>
     </>
-  )
-}
+  );
+};
